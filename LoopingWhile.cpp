6b441f162d022cee2a6ca0eacc39892da8141624
fazila7002 <fazilatunnisa705@gmail.com>
2026-07-21 10:48:27 +0700

    disini dimasukin syarat dari ehile dan dan di masukkan juga si pengacak x atau pengubah nilai x nya

--- LoopingWhile.cpp
+++ LoopingWhile.cpp
@@ -12,6 +12,10 @@ int main (){
     cout << "PERULANGAN WHILE" << endl;
     //Nilai awal x = 0
     x = 1 +rand() %10 ;
+
+    while (x <=5) {
+        cout << "TI Bilangan acak" << x<< endl;
+        x =rand() %10 ; //Pengubah dari nilai x alias pengacaknya
    
 
 
