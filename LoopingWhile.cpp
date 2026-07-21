e01e678fc6fb94944c45e7085bf448c3f3571ce1
fazila7002 <fazilatunnisa705@gmail.com>
2026-07-21 10:45:58 +0700

    tampilan untuk perulangan while  awal

--- LoopingWhile.cpp
+++ LoopingWhile.cpp
@@ -5,8 +5,14 @@ int main (){
     //input var x
     int x;
 
-    
+    //<cstdlib> <ctime>
     srand(time(0));
+
+    //Perulangan While
+    cout << "PERULANGAN WHILE" << endl;
+    //Nilai awal x = 0
+    x = 1 +rand() %10 ;
+   
 
 
     
